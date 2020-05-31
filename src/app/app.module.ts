8a2aa05108637b14cdb97691ddfd074c17cbe66a
Filipe Ferreira <lipe.ferreira1609@hotmail.com>
2020-05-31 10:44:36 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt-BR');
+
 import { AppComponent } from './app.component';
 import {FilesizePipe} from './pipes/filesize.pipe'
+
 
 @NgModule({
   declarations: [
@@ -12,7 +19,9 @@ import {FilesizePipe} from './pipes/filesize.pipe'
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' }    
+  ] ,
   bootstrap: [AppComponent]
 })
 export class AppModule { }
